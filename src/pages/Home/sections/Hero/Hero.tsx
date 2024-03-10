af8b656326a342c26c63ce558d28e502b745ec92
Allan <allanfelipedk@gmail.com>
2024-03-10 15:54:41 -0300

    feat: adding an avatar image and styling components

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -1,16 +1,37 @@
-import { styled } from "@mui/material"
+import { Container, Grid, Typography, styled } from "@mui/material"
+import Avatar from "../../../../assets/images/avatar.jpg"
 
 
 const Hero = () => {
 
     const StyledHero = styled("div")(() => ({
-        backgroundColor: "black"
+        backgroundColor: "black",
+        height: "100vh"
+    }))
+
+    const StyledImg = styled("img")(() => ({
+        width: "100%",
+        borderRadius: "50%",
     }))
 
     return (
         <>
             <StyledHero>
-                Ola
+                <Container>
+                    <Grid container spacing={2}>
+                        <Grid item xs={4}>
+                            <StyledImg src={Avatar} />
+                            xs = 4
+                        </Grid>
+                        <Grid item xs={8}>
+                            <Typography color="primary" variant="h1">Allan Felipe</Typography>
+                            <Typography color="primary" variant="h2">I`m a Software Enginner</Typography>
+                        </Grid>
+
+                    </Grid>
+                </Container>
+
+
             </StyledHero>
         </>
     )
